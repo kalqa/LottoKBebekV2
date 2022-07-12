e70dd9f6eb4dd7127f7feacee193178908873954
Krzycho <k.bebek@wp.pl>
2022-07-12 14:03:22 +0200

    Add class AwardsDisplayer and method printYourAward

---
+++ src/main/java/pl/lotto/AwardsDisplayer.java
@@ -0,0 +1,31 @@
+package pl.lotto;
+
+import java.util.List;
+
+public class AwardsDisplayer {
+
+        public void printYourAward(List<Integer> userNumbers) {
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 0) {
+            System.out.println("Unfortunately, your award is equal zero");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 1) {
+            System.out.println("You won: 0.1 $");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 2) {
+            System.out.println("You won: 2 $");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 3) {
+            System.out.println("You won: 30 $");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 4) {
+            System.out.println("You won: 400 $");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 5) {
+            System.out.println("You won: 5000 $");
+        }
+        if (CollectionsChecker.checkAndCompareCollections(userNumbers) == 6) {
+            System.out.println("You won: 1 000 000 $");
+        }
+
+    }
+}
