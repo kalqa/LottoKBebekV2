aedfec3b02dcdf31fb59ec1fcf282068da2c210c
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2022-07-12 11:02:33 +0200

    refactor

--- src/main/java/pl/lotto/LottoNumberGenerator.java
+++ src/main/java/pl/lotto/LottoNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class LottoNumberGenerator {
 
     @Override
-    public void play(Random random) {
+    public void asdasdasd(Random random) {
         while (randomNumbers.size() < 6) {
             randomNumbers.add(random.nextInt(100));
         }
