33475e869fabae10fb82082df948a9533dccba55
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2022-07-12 10:59:35 +0200

    changed draw to play

--- src/main/java/pl/lotto/LottoNumberGenerator.java
+++ src/main/java/pl/lotto/LottoNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class LottoNumberGenerator {
 
     @Override
-    public void draw(Random random) {
+    public void play(Random random) {
         while (randomNumbers.size() < 6) {
             randomNumbers.add(random.nextInt(100));
         }
