5f1cd0e1dc10091930620e8d1a4a1de9b3abd0e2
Krzycho <k.bebek@wp.pl>
2022-07-13 10:38:58 +0200

    Improve LottoNumberGenerator class

--- src/main/java/pl/lotto/LottoNumberGenerator.java
+++ src/main/java/pl/lotto/LottoNumberGenerator.java
@@ -4,11 +4,14 @@ import java.util.Random;
 import java.util.TreeSet;
 
 public class LottoNumberGenerator {
-    private final TreeSet<Integer> randomNumbers = new TreeSet<>();
-    public void draw(Random random) {
+
+    private static final TreeSet<Integer> randomNumbers = new TreeSet<>();
+    public static void draw(Random random) {
         while (randomNumbers.size() < 6) {
             randomNumbers.add(random.nextInt(100));
         }
     }
-
+    public static TreeSet<Integer> getRandomNumbers() {
+        return randomNumbers;
+    }
 }
