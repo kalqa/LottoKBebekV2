3882db6025dee9cf56da1bb7ae0bb56863a7ad9a
Krzycho <k.bebek@wp.pl>
2022-07-13 10:39:31 +0200

    Main class reduction

--- src/main/java/pl/lotto/Main.java
+++ src/main/java/pl/lotto/Main.java
@@ -1,69 +1,13 @@
 package pl.lotto;
 
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Main {
 
     public static void main(String[] args) {
 
         MessageDisplayer.displayStartMessages();
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        boolean wantToPlay = true;
+        MiniLotek.play();
 
-        while (wantToPlay) {
-            switch (option) {
-                case 1 -> {
-                    play(scanner);
-                    System.out.println("Do you want play again? Yes/No");
-//                    Scanner scanner = new Scanner(System.in);
-                    String answer = scanner.nextLine();
-                    if (answer.equalsIgnoreCase("No")) {
-                        wantToPlay = false;
-                    }
-                }
-                case 2 -> wantToPlay = false;
-
-            }
-        }
-
-    }
-
-    private static void play(Scanner scanner) {
-//        Scanner scanner = new Scanner(System.in);
-        Set<Integer> userNumbers = new TreeSet<>();
-        System.out.println("Please write six numbers:");
-
-        while (userNumbers.size() < 6) {
-            int i = scanner.nextInt();
-            if (!isInRange(i)) {
-                System.out.println("Your number must be between 1-99 inclusive");
-//                throw new BadValueException("Your number must be between 1-99 inclusive");
-            }
-//            if (userNumbers.contains(i)) {
-//                throw new RepeatsValueException("You already wrote the number.");
-//            }
-            userNumbers.add(i);
-//            Collections.sort(userNumbers);
-        }
-
-        System.out.println("Here are your numbers: " + userNumbers);
-
-        MiniLotek miniLotek = new MiniLotek();
-        miniLotek.play(new Random());
-
-        System.out.println("Here are machine numbers: " + miniLotek.getRandomNumbers());
-        System.out.println("You hit accurate: " + miniLotek.checkAndCompareCollections(userNumbers) + " numbers");
-
-        miniLotek.printYourAward(userNumbers);
-    }
-
-    private static boolean isInRange(int numberFromUser) {
-        return numberFromUser >= 1 && numberFromUser <= 99;
     }
 }
 
