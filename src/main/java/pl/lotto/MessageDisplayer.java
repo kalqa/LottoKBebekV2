9e209ee8eab3249f103ebc14ffc3bc17a8c51323
Krzycho <k.bebek@wp.pl>
2022-07-13 11:24:51 +0200

    Improve MessageDisplayer class

--- src/main/java/pl/lotto/MessageDisplayer.java
+++ src/main/java/pl/lotto/MessageDisplayer.java
@@ -3,12 +3,21 @@ package pl.lotto;
 public class MessageDisplayer {
 
     public static void displayStartMessages() {
-        System.out.println("Do you wany to play a MiniLotek? Choose an option below:");
-        System.out.println("1. Play");
-        System.out.println("2. Exit");
+        System.out.println("""
+                Which game do you want to play? Choose an option below:
+                1. Minilotto
+                2. Exit""");
     }
 
     public static void displayYesNoQuestion() {
         System.out.println("Do you want play again? Yes/No");
+    }
+
+    public static void displayNumbers(String message) {
+        System.out.println(message);
+    }
+
+    public static void displayUserScore(String message) {
+        System.out.println(message);
     }
 }
