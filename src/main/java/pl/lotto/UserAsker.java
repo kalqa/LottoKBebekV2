eab5a3e00f50c08bc9800626ed6bcc51c6d19139
Krzycho <k.bebek@wp.pl>
2022-07-13 10:40:36 +0200

    Create UserAsker class

---
+++ src/main/java/pl/lotto/UserAsker.java
@@ -0,0 +1,12 @@
+package pl.lotto;
+
+import java.util.Scanner;
+
+public class UserAsker {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static Scanner askUser() {
+        return scanner;
+    }
+}
