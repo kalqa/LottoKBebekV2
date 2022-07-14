89249b26ac688e3373e73da31615521e12e96e9e
Krzycho <k.bebek@wp.pl>
2022-07-14 11:46:42 +0200

    Rename class

--- src/main/java/pl/lotto/UserNumberPicker.java
+++ src/main/java/pl/lotto/UserNumberPicker.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class UserGameNumberPicker {
+public class UserNumberPicker {
 
     private static final List<Integer> userNumbers = new ArrayList<>();
 
     public static void pickNumbers() {
-        System.out.println("Please write six numbers:");
+        MessageDisplayer.displayMessage("Please write six numbers:");
 
         while (userNumbers.size() < 6) {
             int numberAnswer = UserAsker.askUser().nextInt();
 
             while (!RangeChecker.isInRange(numberAnswer)) {
-                System.out.println("Your number must be between 1-99 inclusive. Please change this number");
+                MessageDisplayer.displayMessage("Your number must be between 1-99 inclusive. Please change this number");
                 numberAnswer = UserAsker.askUser().nextInt();
             }
             while (userNumbers.contains(numberAnswer)) {
-                System.out.println("You already wrote the number. Please change this number");
+                MessageDisplayer.displayMessage("You already wrote the number. Please change this number");
                 numberAnswer = UserAsker.askUser().nextInt();
             }
             userNumbers.add(numberAnswer);
