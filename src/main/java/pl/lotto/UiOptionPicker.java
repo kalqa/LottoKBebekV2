03a6d7b5da7d8c2c70f31df1d9ebf0820ffc1fc0
Krzycho <k.bebek@wp.pl>
2022-07-14 17:19:31 +0200

    Create WantToPlayChecker class and Improve method in UserNumberPicker class

--- src/main/java/pl/lotto/UiOptionPicker.java
+++ src/main/java/pl/lotto/UiOptionPicker.java
@@ -10,19 +10,7 @@ public class UiOptionPicker {
         boolean wantToPlay = true;
         int numericResponse = UserAsker.askUser().nextInt();
         while (wantToPlay) {
-            switch (numericResponse) {
-                case 1 -> {
-                    MiniLottoStarter.runTheGame();
-                    UserAsker.askUser().nextLine();
-                    if (UserAsker.askUser().nextLine().equalsIgnoreCase("No")) {
-                        wantToPlay = false;
-                        GameStarter.chooseGame();
-                    } else {
-                        UserNumberPicker.getUserNumbers().clear();
-                    }
-                }
-                case 2 -> GameStarter.chooseGame();
-            }
+            wantToPlay = WantToPlayChecker.isWantToPlay(wantToPlay, numericResponse);
         }
     }
 }
---
+++ src/main/java/pl/lotto/WantToPlayChecker.java
@@ -0,0 +1,21 @@
+package pl.lotto;
+
+public class WantToPlayChecker {
+
+    public static boolean isWantToPlay(boolean wantToPlay, int numericResponse) {
+        switch (numericResponse) {
+            case 1 -> {
+                MiniLottoStarter.runTheGame();
+                UserAsker.askUser().nextLine();
+                if (UserAsker.askUser().nextLine().equalsIgnoreCase("No")) {
+                    wantToPlay = false;
+                    GameStarter.chooseGame();
+                } else {
+                    UserNumberPicker.getUserNumbers().clear();
+                }
+            }
+            case 2 -> GameStarter.chooseGame();
+        }
+        return wantToPlay;
+    }
+}
