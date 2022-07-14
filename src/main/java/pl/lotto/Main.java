2837375409924812f1f63f9a196b3cccb368c500
Krzycho <k.bebek@wp.pl>
2022-07-14 11:49:03 +0200

    Start application

--- src/main/java/pl/lotto/Main.java
+++ src/main/java/pl/lotto/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MessageDisplayer.displayStartMessages();
+        GameStarter.chooseGame();
 
-        MiniLotek.play();
 
     }
 }
