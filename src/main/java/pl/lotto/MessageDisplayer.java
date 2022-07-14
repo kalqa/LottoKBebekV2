68b7d88f7fc5e945a9a287807f26567d657de1b3
Krzycho <k.bebek@wp.pl>
2022-07-14 11:48:29 +0200

    removal of all methods in favor of one

--- src/main/java/pl/lotto/MessageDisplayer.java
+++ src/main/java/pl/lotto/MessageDisplayer.java
@@ -2,22 +2,7 @@ package pl.lotto;
 
 public class MessageDisplayer {
 
-    public static void displayStartMessages() {
-        System.out.println("""
-                Which game do you want to play? Choose an option below:
-                1. Minilotto
-                2. Exit""");
-    }
-
-    public static void displayYesNoQuestion() {
-        System.out.println("Do you want play again? Yes/No");
-    }
-
-    public static void displayNumbers(String message) {
-        System.out.println(message);
-    }
-
-    public static void displayUserScore(String message) {
+    public static void displayMessage(String message) {
         System.out.println(message);
     }
 }
