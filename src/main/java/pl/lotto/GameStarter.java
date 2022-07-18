679192bc1733af49ad3c157cfc16adcc213cef01
Krzycho <k.bebek@wp.pl>
2022-07-18 08:34:12 +0200

    Renamed message

--- src/main/java/pl/lotto/GameStarter.java
+++ src/main/java/pl/lotto/GameStarter.java
@@ -5,11 +5,11 @@ public class GameStarter {
     public static void chooseGame() {
         MessageDisplayer.displayMessage("""
                 Which game do you want to play? Choose an option below:
-                1. Minilotto
+                1. Lotto
                 2. Exit""");
 
         switch (UserAsker.askUser().nextInt()) {
-            case 1 -> Lotek.startGame();
+            case 1 -> Lotto.startGame();
             case 2 -> System.exit(0);
         }
     }
