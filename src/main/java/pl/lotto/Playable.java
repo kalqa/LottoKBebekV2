4cb9a79814cf0a58cd3b9519337660ece942cf47
Krzycho <k.bebek@wp.pl>
2022-07-18 08:31:19 +0200

    Remove class

--- src/main/java/pl/lotto/Playable.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto;
-
-import java.util.Random;
-
-public interface Playable {
-//    GameResult play(Random random);
-}
--- src/main/java/pl/lotto/RangeChecker.java
+++
@@ -1,8 +0,0 @@
-package pl.lotto;
-
-public class RangeChecker {
-
-    public static boolean isInRange(int numberFromUser) {
-        return numberFromUser >= 1 && numberFromUser <= 99;
-    }
-}
