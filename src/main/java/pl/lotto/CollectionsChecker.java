cf0f9e5b0550589cf423ddafeb821c4254da0b45
Krzycho <k.bebek@wp.pl>
2022-07-18 08:09:13 +0200

    Renamed class method

--- src/main/java/pl/lotto/CollectionsChecker.java
+++ src/main/java/pl/lotto/CollectionsChecker.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class CollectionsChecker {
     public static int checkAndCompareCollections(List<Integer> userNumbers) {
         int sumOfTheSameNumbers = 0;
-        for (int i = 0; i < LottoNumberGenerator.getRandomNumbers().size(); i++) {
-            if (LottoNumberGenerator.getRandomNumbers().contains(userNumbers.get(i))) {
+        for (int i = 0; i < LottoNumberGenerator.getLottoRandomNumbersSet().size(); i++) {
+            if (LottoNumberGenerator.getLottoRandomNumbersSet().contains(userNumbers.get(i))) {
                 sumOfTheSameNumbers++;
             }
         }
